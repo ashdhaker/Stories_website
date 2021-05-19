b140d3e7134d2b676dc2715e87a3976c4e8911c5
ASHD <ashdhaker09@gmail.com>
2021-05-19 15:42:19 +0530

    Intial Commit

---
+++ js/index.js
@@ -0,0 +1,78 @@
+function myFunction() {
+    const x = document.querySelector('#links');
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+
+const story1=document.querySelector("#story1");
+const story2=document.querySelector("#story2");
+const story3=document.querySelector("#story3");
+const story4=document.querySelector("#story4");
+const story5=document.querySelector("#story5");
+const story6=document.querySelector("#story6");
+const story7=document.querySelector("#story7");
+
+
+function displaystory1() {  
+   if(story1.style.display==="block"){
+     story1.style.display='none';
+    
+    }else{
+      story1.style.display='block';
+      location.href='#story1'
+     
+      
+   }
+}
+function displaystory2() {  
+   if(story2.style.display==="block"){
+     story2.style.display='none';
+   }else{
+     story2.style.display='block';
+     location.href='#story2'
+   }
+}
+function displaystory3() {  
+   if(story3.style.display==="block"){
+     story3.style.display='none';
+   }else{
+     story3.style.display='block';
+     location.href='#story3'
+}
+}
+function displaystory4() {  
+   if(story4.style.display==="block"){
+     story4.style.display='none';
+   }else{
+     story4.style.display='block';
+     location.href='#story4'
+   }
+}
+function displaystory5() {  
+   if(story5.style.display==="block"){
+     story5.style.display='none';
+   }else{
+     story5.style.display='block';
+     location.href='#story5'
+   }
+}
+function displaystory6() {  
+   if(story6.style.display==="block"){
+     story6.style.display='none';
+   }else{
+     story6.style.display='block';
+     location.href='#story6'
+   }
+}
+function displaystory7() {  
+   if(story7.style.display==="block"){
+     story7.style.display='none';
+   }else{
+     story7.style.display='block';
+     location.href='#story7'
+   }
+}
